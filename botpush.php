904722fc4fd45c5210bfd02947a8b0c918f37943
fedredz <77093817+fedredz@users.noreply.github.com>
2021-01-08 11:32:46 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -13,7 +13,7 @@ $pushID = 'U297550fa03f223e23724f3a0b70f582b';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$msg = echo "test \n3 \nbuntud
+$msg = echo "test \n3 \nbuntud";
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
