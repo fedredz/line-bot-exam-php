9b87d584ebf6dc401b2d5c04a2f21b83de2b5a76
fedredz <77093817+fedredz@users.noreply.github.com>
2021-01-08 15:07:03 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -13,17 +13,17 @@ $pushID = 'U297550fa03f223e23724f3a0b70f582b';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$msg = "ภาษาไทย";
+$msg = "สวัสดีจ้า";
 $originalimage_url= "https://sv1.picz.in.th/images/2021/01/08/lTo3oa.jpg";
 $previewimage_url= "https://sv1.picz.in.th/images/2021/01/08/lTo3oa.th.jpg";
-//$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
 $imageBuilder = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalimage_url, $previewimage_url);
-//$multiMessageBuilder = new \LINE\LINEBot\MultiMessageBuilder();
- //           $multiMessageBuilder->add($textMessageBuilder);
-  //                               ->add($imageBuilder);
+$multiMessageBuilder = new \LINE\LINEBot\MultiMessageBuilder();
+            $multiMessageBuilder->add($textMessageBuilder);
+                                 ->add($imageBuilder);
 
-//$response = $bot->pushMessage($pushID, $multiMessageBuilder);
+$response = $bot->pushMessage($pushID, $multiMessageBuilder);
 //$response = $bot->pushMessage($pushID, $textMessageBuilder);
-$response = $bot->pushMessage($pushID, $imageBuilder);
+//$response = $bot->pushMessage($pushID, $imageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
